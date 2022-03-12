5eb3cef2df869d97c219507e0030d2fc3aa7dd56
BuzmakovaNatasha <buzmakova_natasha@rambler.ru>
2022-03-13 02:29:36 +0500

    Vue.js Lesson-003
    Создала новое Vue приложение "My personal costs" с помощью Vue CLI.
    Создала 3 компонента: отображения списка расходов, формы добавления новой статьи расходов и пагинации.

---
+++ src/components/PaginationComp.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="pagination">
+    <button @click="prevPage" :disabled="pageNumber == 1">
+      <font-awesome-icon icon="fa-solid fa-angle-left" />
+    </button>
+    <button
+      v-for="n in pageCount"
+      :key="n"
+      @click="onPageNumber(n)"
+      :class="{ active: isPageActive(n) }"
+    >
+      {{ n }}
+    </button>
+    <button @click="nextPage" :disabled="pageNumber >= pageCount">
+      <font-awesome-icon icon="fa-solid fa-angle-right" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaginationComp",
+  data() {
+    return {
+      pageNumber: 1,
+    };
+  },
+  props: {
+    paymentListLength: {
+      type: Number,
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 5,
+    },
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.paymentListLength / this.size);
+    },
+  },
+  methods: {
+    nextPage() {
+      this.pageNumber++;
+      this.$emit("showPageNumber", this.pageNumber);
+    },
+    prevPage() {
+      this.pageNumber--;
+      this.$emit("showPageNumber", this.pageNumber);
+    },
+    onPageNumber(n) {
+      this.pageNumber = n;
+      this.$emit("showPageNumber", this.pageNumber);
+    },
+    isPageActive(n) {
+      return this.pageNumber === n;
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.pagination {
+  display: flex;
+  width: 450px;
+  justify-content: center;
+  border: 1px solid #000;
+  padding: 10px;
+  box-sizing: border-box;
+  & button {
+    border: none;
+    background-color: transparent;
+    margin: 0 5px;
+  }
+  .active {
+    color: #00aaff;
+  }
+}
+</style>
---
+++ src/components/PaymentDisplay.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="list">
+    <div class="item" v-for="item in listShow" :key="item.id">
+      <span class="item__el">{{ item.date }}</span>
+      <span class="item__el">{{ item.category }}</span>
+      <span class="item__el">{{ item.value }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentDisplay",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+    number: {
+      type: Number,
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 5,
+    },
+  },
+  computed: {
+    listShow() {
+      const start = (this.number - 1) * this.size;
+      const end = start + this.size;
+      return this.list.slice(start, end);
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.list {
+  display: flex;
+  flex-direction: column;
+  width: 450px;
+}
+.item {
+  display: flex;
+  text-align: start;
+  padding: 20px 0;
+  position: relative;
+  &::before {
+    content: "";
+    height: 1px;
+    width: 100%;
+    background-color: rgba(75, 75, 75, 0.527);
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  &__el {
+    width: 150px;
+    text-transform: capitalize;
+  }
+}
+</style>
